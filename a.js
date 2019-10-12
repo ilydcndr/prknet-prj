29d1d9f335d89da4f0eda4cee88d302187735390
ilydcndr <ilaydacundar@hotmail.com>
2019-10-12 19:14:45 +0300

    parse

---
+++ a.js
@@ -0,0 +1,12 @@
+
+                          var tarihingunu =(x).value;
+
+                          var k==var tarihingunu
+
+                 for(m=0;m<olaylar.length;m++){
+                       var tarih =OlaylarAyır[m].date;
+                     if(var k= var tarih  ){
+                     }
+                     var eventdiv=document.createElement("div");
+                     var text2=document.createTextNode(OlaylarAyır[k].title)
+                     m++;
